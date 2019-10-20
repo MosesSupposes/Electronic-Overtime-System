68bf902cd80fe966be45e178ebd15ee2be6ef876
Moses Samuel <mosamuel98@gmail.com>
2019-10-20 13:04:43 -0700

    add width and margin to canvas; default "clearOnResize" to false; style "clear" button

--- src/components/Signature.jsx
+++ src/components/Signature.jsx
@@ -6,14 +6,15 @@ import { Button } from 'semantic-ui-react'
 
 // styles
 const SigContainer = styled.div`
-  display: block;
-
-  .sigCanvas {
-      border: 2px solid black;
-  }
+    .sigCanvas {
+        display: block;
+        border: 2px solid black;
+        width: 100%;
+        margin: .25rem 0;
+    }
 `  
 
-export default function Signature(props={width:500, height:200}) {
+export default function Signature(props) {
     let sigCanvas = null
     const setSigCanvas = ref => {
         sigCanvas = ref
@@ -23,15 +24,32 @@ export default function Signature(props={width:500, height:200}) {
         sigCanvas.clear()
     }
 
+    const canvasProps = {
+        className: 'sigCanvas',
+        height: props.height || 200,
+    }
+
     return (
         <>
         <SigContainer>
+            {props.title && 
+                <span><strong>{props.title}</strong></span>
+            }
             <SignatureCanvas 
-                canvasProps={{width: props.width, height: props.height, className: 'sigCanvas'}}
+                canvasProps={canvasProps}
+                clearOnResize={props.clearOnResize || false}
                 ref={setSigCanvas}
             />
+
+            <Button 
+                onClick={clearSignature} 
+                type="button"
+                secondary
+            >
+                Clear
+            </Button>
         </SigContainer>
-        <Button onClick={clearSignature} type="button">Clear</Button>
+
         </>
     )
 }
