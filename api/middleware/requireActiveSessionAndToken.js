f086fedb06c636f0d1f9c2d7221dd96113996b6d
Moses Samuel <mosamuel98@gmail.com>
2019-10-27 16:52:29 -0700

    add middleware for vaildating cookies/tokens

---
+++ api/middleware/requireActiveSessionAndToken.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const { jwtSecret } = require('../config/secrets')
+
+function requireActiveSessionAndToken(req, res, next) {
+    if (req.session && req.session.token) {
+        // verify jwt token
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if (err) {
+                const error = new Error('Invalid token.')
+                error.status = 400
+                next(error)
+            } else {
+                req.user = decodedToken.username
+                next()
+            }
+        })
+    } else {
+        const err = new Error('You need to login to access this enpoint.')
+        err.status = 401
+        next(err)
+    }
+}
+
+module.exports = requireActiveSession
