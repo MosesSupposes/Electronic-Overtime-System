1aefa821971d10517a9e4c5ad189cb27d91888ac
Moses Samuel <mosamuel98@gmail.com>
2019-10-27 17:49:17 -0700

    add a controller for handling user logic

---
+++ api/controllers/UsersController.js
@@ -0,0 +1,49 @@
+const UsersModel = require('../models/UsersModel')
+
+class UsersController {
+    static async allUsers(req, res) {
+        try {
+            const allUsers = await UsersModel.allUsers()
+            res.status(200).json(allUsers)
+        } catch(e) {
+            res.status(500).json({ error: { message: "Internal server error." } })
+        }
+    }
+
+    static async getByUsername(req, res) {
+        try {
+            const user = await UsersModel.findByUsername(req.params.username)
+            res.status(200).json(user)
+        } catch(e) {
+            res.status(404).json({ error: { message: `User ${req.params.username} does not exist.` } })
+        }
+    }
+
+    static async updateByUsername(req, res) {
+        try {
+            const userBeforeUpdate = await UsersModel.findByUsername(req.params.username)
+            try {
+                const userAfterUpdate = await UsersModel.updateByUsername(req.params.username, req.body)
+                res.status(200).json({
+                    beforeUpdate: userBeforeUpdate,
+                    afterUpdate: userAfterUpdate
+                })
+            } catch(e) {
+                res.status(500).json({ error: { message: "Internal server error." } })
+            }
+        } catch(e) {
+            res.status(404).json({ error: { message: `User ${req.params.username} does not exist.` } })
+        }
+    }
+
+    static async deleteByUsername(req, res) {
+        try {
+            await UsersModel.deleteByUsername(req.params.username)
+            res.status(200).json({ success: `Deleted user ${req.params.username}`})
+        } catch(e) {
+            res.status(500).json({ error: { message: `User ${req.params.username} does not exist.` } })
+        }
+    }
+}
+
+module.exports = UsersController
