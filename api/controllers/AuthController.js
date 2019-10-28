c2bc38d854e2b9ad1a7859c8d89350c160e88db4
Moses Samuel <mosamuel98@gmail.com>
2019-10-27 17:11:56 -0700

    add contoller for auth logic

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const AuthModel = require('../models/AuthModel')
+const { jwtSecret } = require('../config/secrets')
+
+
+function generateToken(user) {
+    const payload = {
+        username: user.username,
+        subject: user.id,
+    }
+    const options = { expiresIn: '1h' }
+
+    return jwt.sign(payload, jwtSecret, options)
+}
+
+class AuthController {
+    static async register(req, res) {
+        try {
+            const newUser = await AuthModel.create(req.body)
+            res.status(201).json({
+                success: `Welcome ${newUser.username}!`,
+                user: newUser,
+                token: generateToken(newUser)
+            })
+        } catch(e) {
+            res.status(400).json({ error: { message: "A user with this name already exists." } })
+        }
+    }
+
+    static async login(req, res) {
+        try {
+            const user = await AuthModel.findByUsername(req.body.username)
+            bcrypt.compare(req.body.password, user.password, (err, passwordsMatch) => {
+                if (err) res.status(500).json({ error: { message: 'Internal server error.' } })
+                else if (!passwordsMatch) res.status(400).json({ error: { message: "Invalid password." } })
+                else res.status(200).json({
+                    success: `Welcome ${newUser.username}!`,
+                    user,
+                    token: generateToken(user)
+                })
+            })
+        } catch(e) {
+            res.status(500).json({ error: { message: "Invalid credentials." } })
+        }
+    }
+
+    static async logout(req, res) {
+        req.session.destroy(err => {
+            if (err) res.status(500).json({ error: { message: "Internal server error." } })
+            else res.status(200).json({message: "Goodbye! 👋🏾"})
+        })
+    }
+}
+
+module.exports = AuthController
