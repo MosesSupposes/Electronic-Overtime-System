a4f5b422abbf1d0661229142c9b09171785f3ac4
Moses Samuel <mosamuel98@gmail.com>
2019-11-03 22:19:23 -0800

    remove node-env endpoint

--- api/server.js
+++ api/server.js
@@ -53,11 +53,6 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' })
 })
 
-// test -- this should be removed
-server.get('/node-env', (req, res) => {
-  res.status(200).json({ env: process.env.NODE_ENV})
-})
-
 server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
 
