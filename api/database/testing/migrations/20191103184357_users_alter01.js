3897bbd1f0ec2a90eae654eca0734d989d300abf
Moses Samuel <mosamuel98@gmail.com>
2019-11-03 18:53:30 -0800

    add email and role columns to the users table

---
+++ api/database/testing/migrations/20191103184357_users_alter01.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.table('users', tbl => {
+        tbl.string('email')
+        // Roles are employee, supervisor, senior supervisor, admin
+        tbl.string('role').defaultTo('employee')
+    })
+  };
+  
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users')
+};
