37769c323154975145598535979e88f521cb8249
Moses Samuel <mosamuel98@gmail.com>
2019-11-04 20:53:07 -0800

    add an input field for email on register form

--- client/src/components/forms/Register.jsx
+++ client/src/components/forms/Register.jsx
@@ -12,6 +12,7 @@ export default function Register(props) {
 
     const initialStateCredentials = {
         username: '',
+        email: '',
         password: '',
         cPassword: '',
     }
@@ -23,9 +24,10 @@ export default function Register(props) {
     }
 
     const validateInputs = (inputs) => {
-        const { username, password, cPassword } = inputs
+        const { username, email, password, cPassword } = inputs
 
         if (!username) setErrors((prevErrors) => ({...prevErrors, username: 'You must enter a username.'}))
+        if (!email) setErrors((prevErrors) => ({ ...prevErrors, email: 'You must enter an email address.'}))
         if (password.length < 6) setErrors((prevErrors) => ({...prevErrors, password: 'Your password must be at least 6 characters.'}))
         if (!password) setErrors((prevErrors) => ({...prevErrors, password: 'You must enter a password.'}) )
         if (password !== cPassword) setErrors((prevErrors) => ({...prevErrors, cPassword: 'Passwords must match.'}))
@@ -38,14 +40,14 @@ export default function Register(props) {
         // display any new errors 
         validateInputs(newUserCredentials)
 
-        const { username, password, cPassword } = newUserCredentials
+        const { username, email, password, cPassword } = newUserCredentials
 
         if (username &&
             password &&
             password.length >=6 && 
             password === cPassword
         ) {
-            axios.post('https://electronic-overtime-system.herokuapp.com/api/auth/register', { username, password })
+            axios.post('https://electronic-overtime-system.herokuapp.com/api/auth/register', { username, password, email })
                 .then(res => { 
                     console.log(res)
                     // TODO: save username and role to global state via react-conflux 
@@ -62,13 +64,11 @@ export default function Register(props) {
     const [newUserCredentials,, handleChanges, handleSubmit] = useForm(initialStateCredentials, handleSubmitCb)
     const [errors, setErrors] = React.useState(initialStateErrors)
 
-
     return (
         <Container>
             <h2>Register</h2>
 
             <Form error onSubmit={handleSubmit}>
-                    {/* <label>Username</label> */}
                     <Form.Input 
                         name="username" 
                         label="Username"
@@ -76,6 +76,16 @@ export default function Register(props) {
                         type="text"
                         placeholder="enter your username"
                         value={newUserCredentials.username}
+                        onChange={handleChanges}
+                    />
+
+                    <Form.Input 
+                        name="email"
+                        label="Email"
+                        error={errors.email && { content: errors.email, pointing: "below" }}
+                        type="email"
+                        placeholder="someone@example.com"
+                        value={newUserCredentials.email}
                         onChange={handleChanges}
                     />
 
