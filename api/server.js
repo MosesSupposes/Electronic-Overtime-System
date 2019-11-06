cd87f1df4f7e8ff722b678af2f4a11d519318013
Moses Samuel <mosamuel98@gmail.com>
2019-11-05 19:43:55 -0800

    allow cookies to be stored over http

--- api/server.js
+++ api/server.js
@@ -21,7 +21,8 @@ const sessionConfiguration = {
     cookie: {
       httpOnly: process.env.NODE_ENV === 'production' ? true : false,
       maxAge: 1000 * 60 * 60, // sessions last for one hour
-      secure: process.env.NODE_ENV === 'production' ? true : false,
+      // secure: process.env.NODE_ENV === 'production' ? true : false,
+      secure: false
     },
     resave: false,
     saveUninitialized: true,
