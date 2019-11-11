895f41bb38981a3d139b41355ed1990c54c818ef
Moses Samuel <mosamuel98@gmail.com>
2019-11-10 16:09:56 -0800

    (attempt to) render the EmployeeDashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+
 import './App.css'
+
+import Elm from 'react-elm-components'
+
 import OvertimeForm from './components/forms/Overtime'
 import Register from './components/forms/Register'
 import Login from './components/forms/Login'
 import AllUsers from './components/AllUsers'
+import EmployeeDashboard from './components/dashboards/EmployeeDashboard'
 
 function App() {
   return (
@@ -19,6 +24,7 @@ function App() {
 
       <h2>Employee Dashboard</h2>
 
+      <Route path="/" render={(props) => <Elm src={EmployeeDashboard} />} />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route path="/forms/overtime" component={OvertimeForm} />
